1210e166d61f99c6c710f7afcba9ce58682a6773
Luis Armendariz <luis.armendariz@gmail.com>
2017-03-19 10:02:34 -0700

    Added get-category-pages.py

---
+++ get-category-pages.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import sys
+import json
+import wiki
+
+# Example: https://en.wikipedia.org/w/api.php?action=query&format=json&list=categorymembers&cmtype=page&cmtitle=Category:Heart
+
+def main():
+    if len(sys.argv) < 2:
+        # NOTE: The category can be specified with either spaces or underscores.
+        sys.stderr.write("{0} CATEGORY\n".format(sys.argv[0]))
+        sys.exit(1)
+
+    category = sys.argv[1]
+
+    wiki_api = wiki.API('wikipedia')
+    #ganfyd_api = wiki.API('ganfyd')
+
+    #obj = wiki_api.query_category_pages(category)
+    obj = wiki_api.get_category_pages(category)
+
+    json.dump(obj, sys.stdout, indent=2)
+    sys.stdout.write('\n')
+
+if __name__ == "__main__":
+    main()
